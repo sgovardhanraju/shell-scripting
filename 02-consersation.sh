772fa0bc7ac21e427386dbad29e7b1906b77f674
Govardhan Raju <gsurapur@gmail.com>
2025-09-28 14:45:31 +0400

    variables

--- 02-consersation.sh
+++ 02-consersation.sh
@@ -2,5 +2,5 @@
 
 echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: I am fine, thank you."
-echo "suresh :: Hi Ramesh, How are you?"
+echo "Suresh :: Hi Ramesh, How are you?"
 echo "Ramesh ::Hi Suresh I am thank you"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: I am fine, thank you."
+echo "$PERSON2:: Hi $PERSON1, How are you?"
+echo "$PERSON1:: Hi $PERSON2 I am thank you"
