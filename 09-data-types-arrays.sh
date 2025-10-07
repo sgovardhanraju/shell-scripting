67a6ec6cbc669c94c1d12a0fb560f0547a4b1e92
Govardhan Raju <gsurapur@gmail.com>
2025-10-07 05:36:14 +0400

    shell

--- 09-data-types-arrays.sh
+++ 09-data-types-arrays.sh
@@ -2,3 +2,7 @@
 
 FAMILY=("Govardhan" "Sowjanya" "Sowmya" "Jasvitha")
 echo "All Family members : ${FAMILY[@]}"
+echo "First member : ${FAMILY[0]}"
+echo "Second member : ${FAMILY[1]}"
+echo "Third member:${FAMILY[2]}"
+echo "Fourth member : ${FAMILY[3]}"
