b306ed3b915c52d2070b2818ff5f94f7506baee6
Govardhan Raju <gsurapur@gmail.com>
2025-10-08 16:14:41 +0400

    shell

--- 08-installation.sh
+++ 08-installation.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run the script with root previleges"
+    echo "ERROR:: Please run the script with root privileges"
     exit 1
 fi
 dnf install mysql -y
