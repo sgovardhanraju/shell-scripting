2693c8b118428eb2db2ab4c107b0015c33680bf3
Govardhan Raju <gsurapur@gmail.com>
2025-10-11 00:18:04 +0400

    shell

--- 19-set.sh
+++ 19-set.sh
@@ -3,11 +3,12 @@
 set -e
 
 error(){
-
+    echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
 }
+
 trap error ERR
 
 echo "Hello.."
 echo "Before error"
-asdfasf:;;;dasd #here shell understatnds there is an error and signal is ERR 
+asdfasf:;dasd #here shell understatnds there is an error and signal is ERR 
 echo "After error"
