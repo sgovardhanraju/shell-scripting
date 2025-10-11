6b04313ccdfc5a1c9689c44259b686632c5ac663
Govardhan Raju <gsurapur@gmail.com>
2025-10-11 16:10:52 +0400

    session-19

---
+++ 20-Script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME=INDAI
+echo "My country :$NAME"
+echo "PID of Script1: $$"
+sh 21-Script2.sh
---
+++ 21-Script2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+NAME=INDAI
+echo "My country :$NAME"
+echo "PID of Script2: $$"
